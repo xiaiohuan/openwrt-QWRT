d4522191f83052a09fab6a6ae06ab34701042586
小丶浣 <78002376+xiaiohuan@users.noreply.github.com>
2023-04-11 10:01:06 +0800

    Update generic.mk
    Signed-off-by: 小丶浣 <78002376+xiaiohuan@users.noreply.github.com>

--- target/linux/ipq807x/image/generic.mk
+++ target/linux/ipq807x/image/generic.mk
@@ -107,7 +107,7 @@ define Device/redmi_ax6
 	$(call Device/xiaomi_ax3600)
 	DEVICE_VENDOR := Redmi
 	DEVICE_MODEL := AX6
-	DEVICE_PACKAGES := ipq-wifi-redmi_ax6
+	DEVICE_PACKAGES := ipq-wifi-redmi_ax6 uboot-envtools
 endef
 TARGET_DEVICES += redmi_ax6
 
